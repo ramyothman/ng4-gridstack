dafa847fa209685af3df5a7ff9d18713ba57aa34
Ramy Othman <ramy.othman.1984@gmail.com>
2017-10-01 16:49:50 +0200

    update coordinates

--- src/grid-stack-item.component.ts
+++ src/grid-stack-item.component.ts
@@ -4,8 +4,8 @@ declare var jQuery: any;
 declare var _: any;
 
 @Component({
-  selector: 'grid-stack-item',
-  template: `<div class="grid-stack-item-content">
+    selector: 'grid-stack-item',
+    template: `<div class="grid-stack-item-content">
               <div #contentPlaceholder *ngIf="contentTemplate"></div>
               <ng-content *ngIf="!contentTemplate"></ng-content>
             </div>`
@@ -15,66 +15,69 @@ export class GridStackItemComponent implements OnInit, OnDestroy, AfterViewInit 
     @Input() contentTemplate: string;
     @Input() option: GridStackItem;
     @Output() onGridConfigurationChanged = new EventEmitter<GridStackItem>();
-    
+
     contentComponentRef: ComponentRef<any> = null;
     jGridRef: any = null;
     public jWidgetRef: any = null;
-    
-    constructor(private el: ElementRef, private renderer: Renderer) {}
+
+    constructor(private el: ElementRef, private renderer: Renderer) {
+        this.jWidgetRef = el.nativeElement;
+    }
     get nativeElement(): HTMLElement {
         return this.el.nativeElement;
     }
-  ngOnInit() {
-      this.RenderWidget(null);
-  }
+    ngOnInit() {
+        this.RenderWidget(null);
+    }
 
-  RenderWidget(item: GridStackItem) {
-      let renderer = this.renderer;
-      if(item != null)
-          this.option = item;
+    RenderWidget(item: GridStackItem) {
+        let renderer = this.renderer;
+        if (item != null)
+            this.option = item;
 
-      //this.renderer.setElementAttribute(this.nativeElement, "style", "margin-left:" + this.option.marginWidth + ";");
-      //this.renderer.setElementAttribute(this.nativeElement, "data-gs-x", String(this.option.x));
-      //this.renderer.setElementAttribute(this.nativeElement, "data-gs-y", String(this.option.y));
-      //this.renderer.setElementAttribute(this.nativeElement, "data-gs-width", String(this.option.width));
-      //this.renderer.setElementAttribute(this.nativeElement, "data-gs-height", String(this.option.height));
-      //if (this.option.minWidth) {
-      //    renderer.setElementAttribute(this.nativeElement, "data-gs-min-width", String(this.option.minWidth));
-      //}
-      //if (this.option.noResize != null && this.option.noResize == true) {
-      //    renderer.setElementAttribute(this.nativeElement, "data-gs-no-resize", "yes");
-      //}
+        //this.renderer.setElementAttribute(this.nativeElement, "style", "margin-left:" + this.option.marginWidth + ";");
+        //this.renderer.setElementAttribute(this.nativeElement, "data-gs-x", String(this.option.x));
+        //this.renderer.setElementAttribute(this.nativeElement, "data-gs-y", String(this.option.y));
+        //this.renderer.setElementAttribute(this.nativeElement, "data-gs-width", String(this.option.width));
+        //this.renderer.setElementAttribute(this.nativeElement, "data-gs-height", String(this.option.height));
+        //if (this.option.minWidth) {
+        //    renderer.setElementAttribute(this.nativeElement, "data-gs-min-width", String(this.option.minWidth));
+        //}
+        //if (this.option.noResize != null && this.option.noResize == true) {
+        //    renderer.setElementAttribute(this.nativeElement, "data-gs-no-resize", "yes");
+        //}
 
-  }
+    }
 
-  update(x: number, y: number, width: number, height: number): void {
-      if (x === this.option.x && y === this.option.y && width === this.option.width && height === this.option.height)
-          return;
-      if (this.option != null) {
-          this.option.x = x;
-          this.option.y = y;
-          this.option.width = width;
-          this.option.height = height;
+    update(x: number, y: number, width: number, height: number): void {
+        // console.log("here");
+        if (x === this.option.x && y === this.option.y && width === this.option.width && height === this.option.height)
+            return;
+        if (this.option != null) {
+            this.option.x = x;
+            this.option.y = y;
+            this.option.width = width;
+            this.option.height = height;
 
-          var optionNew = GridStackItem.Clone(this.option);
-          this.onGridConfigurationChanged.emit(optionNew);
-      }
-  }
+            var optionNew = GridStackItem.Clone(this.option);
+            this.onGridConfigurationChanged.emit(optionNew);
+        }
+    }
 
-  ngAfterViewInit(): void {
-      //if (!!this.contentTemplate) {
-      //    this.componentService.getDynamicComponentFactory({
-      //        selector: `grid-stack-item-${Date.now()}`,
-      //        template: this.contentTemplate
-      //    })
-      //        .then(factory => {
-      //            this.contentComponentRef = this.contentPlaceholder.createComponent(factory);
-      //        })
-      //}
-  }
+    ngAfterViewInit(): void {
+        //if (!!this.contentTemplate) {
+        //    this.componentService.getDynamicComponentFactory({
+        //        selector: `grid-stack-item-${Date.now()}`,
+        //        template: this.contentTemplate
+        //    })
+        //        .then(factory => {
+        //            this.contentComponentRef = this.contentPlaceholder.createComponent(factory);
+        //        })
+        //}
+    }
 
-  ngOnDestroy(): void {
-      if (this.contentComponentRef !== null)
-          this.contentComponentRef.destroy();
-  }
+    ngOnDestroy(): void {
+        if (this.contentComponentRef !== null)
+            this.contentComponentRef.destroy();
+    }
 }
--- src/grid-stack.component.ts
+++ src/grid-stack.component.ts
@@ -6,85 +6,90 @@ declare var jQuery: any;
 declare var _: any;
 
 @Component({
-  selector: 'grid-stack',
-  template: `<ng-content></ng-content>`,
-  styles: [":host { display: block; }"]
+    selector: 'grid-stack',
+    template: `<ng-content></ng-content>`,
+    styles: [":host { display: block; }"]
 })
 export class GridStackComponent implements AfterContentInit {
-  @Input() options: GridStackOptions = new GridStackOptions();
-  @ContentChildren(GridStackItemComponent) items: QueryList<GridStackItemComponent>;
-  private gridStack: any = null;
-  private grid: any = null;
-  constructor(private el: ElementRef, private renderer: Renderer) {
-      
-  }
+    @Input() options: GridStackOptions = new GridStackOptions();
+    @ContentChildren(GridStackItemComponent) items: QueryList<GridStackItemComponent>;
+    private gridStack: any = null;
+    private grid: any = null;
+    constructor(private el: ElementRef, private renderer: Renderer) {
 
-  makeWidget(item: GridStackItemComponent) {
-      //let widget = this.grid.makeWidget(item.nativeElement);
-      item.jGridRef = this.grid;
-      //item.jWidgetRef = widget;
-      if (item.option != null && item.option.noResize != null && item.option.noResize == true)
-          return;
-      this.grid.resizable(item.nativeElement, true);
-      this.grid.move(item.nativeElement, item.option.x, item.option.y);
-      this.grid.resize(item.nativeElement, item.option.width, item.option.height);
-  };
+    }
 
-  public AddWidget(item: GridStackItemComponent) {
-      let widget = this.grid.makeWidget(item.nativeElement);
-      item.jGridRef = this.grid;
-      //item.jWidgetRef = widget;
-      if (item.option != null && item.option.noResize != null && item.option.noResize == true)
-          return;
-      this.grid.resizable(item.nativeElement, true);
-      this.grid.move(item.nativeElement, item.option.x, item.option.y);
-      this.grid.resize(item.nativeElement, item.option.width, item.option.height);
-  }
+    makeWidget(item: GridStackItemComponent) {
+        //let widget = this.grid.makeWidget(item.nativeElement);
+        item.jGridRef = this.grid;
+        //item.jWidgetRef = widget;
+        if (item.option != null && item.option.noResize != null && item.option.noResize == true)
+            return;
+        this.grid.resizable(item.nativeElement, true);
+        this.grid.move(item.nativeElement, item.option.x, item.option.y);
+        this.grid.resize(item.nativeElement, item.option.width, item.option.height);
+    };
 
-  public RemoveWidget(item: GridStackItemComponent) {
-      let widget = this.grid.removeWidget(item.nativeElement, false);
-  }
-  ngAfterContentInit(): void {
-      var that = this;
-      let nativeElement = this.el.nativeElement;
-      if (this.options == null)
-          this.options = new GridStackOptions();
-      if (this.options.cellHeight == null)
-        this.options.cellHeight = "60px";
-      if (this.options.width == null)
-          this.options.width = 12;
-      if (this.options.height == null)
-          this.options.height = 0;
-      if (this.options.animate == null)
-          this.options.animate = true;
-      if (this.options.float == null)
-          this.options.float = false;
-      if (this.options.resizable == null)
-          this.options.resizable = true;
-      this.renderer.setElementAttribute(nativeElement, "data-gs-width", String(this.options.width));
-      this.renderer.setElementAttribute(nativeElement, "data-gs-height", String(this.options.height));
-      
-      this.gridStack = jQuery(nativeElement).gridstack(this.options);
-      this.grid = this.gridStack.data("gridstack");
+    public AddWidget(item: GridStackItemComponent) {
+        let widget = this.grid.makeWidget(item.nativeElement);
+        item.jGridRef = this.grid;
+        //item.jWidgetRef = widget;
+        if (item.option != null && item.option.noResize != null && item.option.noResize == true)
+            return;
+        this.grid.resizable(item.nativeElement, true);
+        this.grid.move(item.nativeElement, item.option.x, item.option.y);
+        this.grid.resize(item.nativeElement, item.option.width, item.option.height);
+    }
 
-      this.gridStack.on("change", (e: any, items: any) => {
-          _.each(items, (item: any) => this.widgetChanged(item));
-      });
+    public RemoveWidget(item: GridStackItemComponent) {
+        let widget = this.grid.removeWidget(item.nativeElement, false);
+    }
+    ngAfterContentInit(): void {
+        var that = this;
+        let nativeElement = this.el.nativeElement;
+        if (this.options == null)
+            this.options = new GridStackOptions();
+        if (this.options.cellHeight == null)
+            this.options.cellHeight = "60px";
+        if (this.options.width == null)
+            this.options.width = 12;
+        if (this.options.height == null)
+            this.options.height = 0;
+        if (this.options.animate == null)
+            this.options.animate = true;
+        if (this.options.float == null)
+            this.options.float = false;
+        if (this.options.resizable == null)
+            this.options.resizable = true;
+        this.renderer.setElementAttribute(nativeElement, "data-gs-width", String(this.options.width));
+        this.renderer.setElementAttribute(nativeElement, "data-gs-height", String(this.options.height));
 
-      
-      
+        this.gridStack = jQuery(nativeElement).gridstack(this.options);
+        this.grid = this.gridStack.data("gridstack");
 
-      // Initialize widgets
-      this.items.forEach(item => that.makeWidget(item));
+        this.gridStack.on("change", (e: any, items: any) => {
+            _.each(items, (item: any) => this.widgetChanged(item));
+        });
 
-  }
 
-  private widgetChanged(change: GridStackItem): void {
-      var jWidget = change.el;
-      var gridStackItem = this.items.find(item => item.jWidgetRef !== null ? item.jWidgetRef[0] === jWidget[0] : false);
-      if (!gridStackItem)
-          return;
-      
-      gridStackItem.update(change.x, change.y, change.width, change.height);
-  }
+
+
+        // Initialize widgets
+        this.items.forEach(item => that.makeWidget(item));
+
+    }
+
+    private widgetChanged(change: GridStackItem): void {
+        console.log(change);
+        //  debugger;
+        //console.log(change);
+        var jWidget = change.el;
+        var gridStackItem = this.items.find(item => item.jWidgetRef !== null ? item.jWidgetRef === jWidget[0] : false);
+        if (!gridStackItem)
+            return;
+
+
+        gridStackItem.update(change.x, change.y, change.width, change.height);
+
+    }
 }
